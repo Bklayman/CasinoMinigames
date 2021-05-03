ba20fa5f676bb59e727a7d8200efd8729916e06f
Brendan Klayman <bklayma1@binghamton.edu>
2021-05-03 10:08:11 -0400

    Some work on blackjack and again trying to fix image error on texas holdem.

--- Casino Minigames/Casino Minigames/Blackjack.h
+++ Casino Minigames/Casino Minigames/Blackjack.h
@@ -5,6 +5,24 @@
 
 @interface Blackjack : UIViewController
 
+@property (nonatomic, strong) IBOutlet UIImageView* playerCard1;
+@property (nonatomic, strong) IBOutlet UIImageView* playerCard2;
+@property (nonatomic, strong) IBOutlet UIImageView* playerCard3;
+@property (nonatomic, strong) IBOutlet UIImageView* playerCard4;
+@property (nonatomic, strong) IBOutlet UIImageView* playerCard5;
+@property (nonatomic, strong) IBOutlet UIImageView* playerCard6;
+@property (nonatomic, strong) IBOutlet UIImageView* playerCard7;
+@property (nonatomic, strong) IBOutlet UIImageView* playerCard8;
+
+@property (nonatomic, strong) IBOutlet UIImageView* dealerCard1;
+@property (nonatomic, strong) IBOutlet UIImageView* dealerCard2;
+@property (nonatomic, strong) IBOutlet UIImageView* dealerCard3;
+@property (nonatomic, strong) IBOutlet UIImageView* dealerCard4;
+@property (nonatomic, strong) IBOutlet UIImageView* dealerCard5;
+@property (nonatomic, strong) IBOutlet UIImageView* dealerCard6;
+@property (nonatomic, strong) IBOutlet UIImageView* dealerCard7;
+@property (nonatomic, strong) IBOutlet UIImageView* dealerCard8;
+
 @end
 
 #endif
--- Casino Minigames/Casino Minigames/Blackjack.m
+++ Casino Minigames/Casino Minigames/Blackjack.m
@@ -40,6 +40,28 @@
     return result;
 }
 
+- (UIImageView*)getPlayerCardImage:(int)index{
+    switch(index){
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+    }
+    return NULL;
+}
+
 - (NSMutableArray*)getHandValue:(NSMutableArray*)hand{
     return [self handValueHelper:0 :hand :0];
 }
