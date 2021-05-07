904d48bed5bb5c1599b82a95d0918b216b93b06e
rgirong1 <rgironguevara6940@bths.edu>
2021-05-07 11:23:18 -0400

    Added Main menu background and buttons

--- Casino Minigames/Casino Minigames/ViewController.swift
+++ Casino Minigames/Casino Minigames/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var holdemButton: UIButton!
+    @IBOutlet var blackjackButton: UIButton!
+    @IBOutlet var slotButton: UIButton!
+    @IBOutlet var horseButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        casinoBackground();
+        
+        holdemButton.frame = CGRect(x: 7, y: 300, width: 400, height: 100)
+        blackjackButton.frame = CGRect(x: 7, y: 420, width: 400, height: 100)
+        slotButton.frame = CGRect(x: 7, y: 540, width: 400, height: 100)
+        horseButton.frame = CGRect(x: 7, y: 660, width: 400, height: 100)
     }
 
+    func casinoBackground(){
+        let background = UIImage(named: "Casino_bg.jpg")
+
+        var casinoBGView : UIImageView!
+        casinoBGView = UIImageView(frame: view.bounds)
+        casinoBGView.contentMode =  UIView.ContentMode.scaleAspectFill
+        casinoBGView.clipsToBounds = true
+        casinoBGView.image = background
+        casinoBGView.center = view.center
+        view.addSubview(casinoBGView)
+        self.view.sendSubviewToBack(casinoBGView)
+    }
 
 }
 
